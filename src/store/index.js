add1f8f4d849b9d24d89f3a4a7758419780aa2c7
Axel Gromat <axel.gromat@gmail.com>
2021-04-19 02:32:44 +0200

    Fixed a missing import on persisted state

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import router from '../router/index'
 import axios from 'axios'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
